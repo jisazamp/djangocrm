9c28114b64bfd996593ce8c07feffefbd03ef241
Juan Pablo Isaza M. <jisaza@vaovatravel.com>
2024-09-24 19:31:01 -0500

    login

--- website/views.py
+++ website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home_page(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Has inciado sesión de manera éxitosa")
+            return redirect("home")
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos. Revisa tus datos, e intenta de nuevo.")
+            return redirect("home")
+
     return render(request, "home_page.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Has cerrado sesión de manera exitosa")
+    return redirect("home")
